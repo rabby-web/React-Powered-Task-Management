7ded4a374a436fc938f0c9a2866b7cb3c1a03955
rabby-web <rabby.webdeveloper@gmail.com>
2024-01-16 21:57:16 +0600

    dainamc UI setup the list

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -1,21 +1,27 @@
+import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
 
 export default function TaskBoard() {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "learn react",
+    description: "abcd abcd abcd",
+    tags: ["web", "react", "js"],
+    priority: "high",
+    isFavorite: true,
+  };
+  const [tasks, setTasks] = useState([defaultTask]);
+
   return (
     <>
       <section className="mb-20" id="tasks">
         <div className="container">
-          {/* <!-- Search Box --> */}
           <SearchTask />
-          {/* <!-- Search Box Ends --> */}
           <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-            {/* task action */}
             <TaskActions />
-            {/* task action ends */}
-            {/* task list */}
-            <TaskList />
+            <TaskList tasks={tasks} />
           </div>
         </div>
       </section>
