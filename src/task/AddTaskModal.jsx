77e3da2bb9f44d1be5ba370299a34a372fb44f8b
rabby-web <rabby.webdeveloper@gmail.com>
2024-01-17 11:13:57 +0600

    Add task

--- src/task/AddTaskModal.jsx
+++ src/task/AddTaskModal.jsx
@@ -1,4 +1,26 @@
-export default function AddTaskModal() {
+import { useState } from "react";
+
+export default function AddTaskModal({ onSave }) {
+  const [task, setTask] = useState({
+    id: crypto.randomUUID(),
+    title: "",
+    description: "",
+    tags: [],
+    priority: "",
+    isFavorite: false,
+  });
+  const handleChange = (evt) => {
+    // evt.preventDefault();
+    const name = evt.target.name;
+    let value = evt.target.value;
+    if (name === "tags") {
+      value = value.split(",");
+    }
+    setTask({
+      ...task,
+      [name]: value,
+    });
+  };
   return (
     <>
       <div className="bg-black  bg-opacity-70 h-full w-full z-20 absolute top-0 left-0 p-5">
@@ -15,6 +37,8 @@ export default function AddTaskModal() {
                 type="text"
                 name="title"
                 id="title"
+                value={task.title}
+                onChange={handleChange}
                 required
               />
             </div>
@@ -25,6 +49,8 @@ export default function AddTaskModal() {
                 type="text"
                 name="description"
                 id="description"
+                value={task.description}
+                onChange={handleChange}
                 required
               ></textarea>
             </div>
@@ -36,6 +62,8 @@ export default function AddTaskModal() {
                   type="text"
                   name="tags"
                   id="tags"
+                  value={task.tags}
+                  onChange={handleChange}
                   required
                 />
               </div>
@@ -45,12 +73,14 @@ export default function AddTaskModal() {
                   className="block w-full cursor-pointer rounded-md bg-[#2D323F] px-3 py-2.5"
                   name="priority"
                   id="priority"
+                  value={task.priority}
+                  onChange={handleChange}
                   required
                 >
                   <option value="">Select Priority</option>
-                  <option value="low">Low</option>
-                  <option value="medium">Medium</option>
-                  <option value="high">High</option>
+                  <option value="Low">Low</option>
+                  <option value="Medium">Medium</option>
+                  <option value="High">High</option>
                 </select>
               </div>
             </div>
@@ -58,9 +88,10 @@ export default function AddTaskModal() {
           <div className="mt-16 flex justify-center lg:mt-20">
             <button
               type="submit"
+              onClick={() => onSave(task)}
               className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
             >
-              Create new Task
+              Save
             </button>
           </div>
         </form>
--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -16,14 +16,16 @@ export default function TaskBoard() {
   const [tasks, setTasks] = useState([defaultTask]);
   const [showAddModal, setShowAddModal] = useState(false);
 
-  function handleAddTask() {
-    console.log("click");
+  function handleAddTask(newTask) {
+    console.log("click", newTask);
+    setTasks([...tasks, newTask]);
+    setShowAddModal(false);
   }
 
   return (
     <>
       <section className="mb-20" id="tasks">
-        {showAddModal && <AddTaskModal />}
+        {showAddModal && <AddTaskModal onSave={handleAddTask} />}
         <div className="container">
           <SearchTask />
           <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
