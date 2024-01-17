8c21f4525e18570e9c74e2e0ca4a01d9bf54d70f
rabby-web <rabby.webdeveloper@gmail.com>
2024-01-17 16:32:55 +0600

    task add and edit

--- src/task/AddTaskModal.jsx
+++ src/task/AddTaskModal.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 
-export default function AddTaskModal({ onSave }) {
-  const [task, setTask] = useState({
-    id: crypto.randomUUID(),
-    title: "",
-    description: "",
-    tags: [],
-    priority: "",
-    isFavorite: false,
-  });
+export default function AddTaskModal({ onSave, taskToUpdate }) {
+  const [task, setTask] = useState(
+    taskToUpdate || {
+      id: crypto.randomUUID(),
+      title: "",
+      description: "",
+      tags: [],
+      priority: "",
+      isFavorite: false,
+    }
+  );
+  const [isAdd, setIsAdd] = useState(Object.is(taskToUpdate, null));
   const handleChange = (evt) => {
     // evt.preventDefault();
     const name = evt.target.name;
@@ -26,7 +29,7 @@ export default function AddTaskModal({ onSave }) {
       <div className="bg-black  bg-opacity-70 h-full w-full z-20 absolute top-0 left-0 p-5">
         <form className=" mx-auto my-10 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11">
           <h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px] z-10 absolute top-1/4 left-1/3">
-            Add New Task
+            {isAdd ? "Add New Task" : "Edit Task"}
           </h2>
 
           <div className="space-y-9 text-white lg:space-y-10">
@@ -88,7 +91,7 @@ export default function AddTaskModal({ onSave }) {
           <div className="mt-16 flex justify-center lg:mt-20">
             <button
               type="submit"
-              onClick={() => onSave(task)}
+              onClick={() => onSave(task, isAdd)}
               className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
             >
               Save
--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -15,22 +15,44 @@ export default function TaskBoard() {
   };
   const [tasks, setTasks] = useState([defaultTask]);
   const [showAddModal, setShowAddModal] = useState(false);
+  const [taskToUpdate, setTaskToUpdate] = useState(null);
 
-  function handleAddTask(newTask) {
-    console.log("click", newTask);
-    setTasks([...tasks, newTask]);
+  function handleAddEditTask(newTask, isAdd) {
+    if (isAdd) {
+      setTasks([...tasks, newTask]);
+    } else {
+      setTasks(
+        tasks.map((task) => {
+          if (task.id === newTask.id) {
+            return newTask;
+          }
+          return task;
+        })
+      );
+    }
+
     setShowAddModal(false);
+  }
+  function handleEditTask(task) {
+    console.log("click edit");
+    setTaskToUpdate(task);
+    setShowAddModal(true);
   }
 
   return (
     <>
       <section className="mb-20" id="tasks">
-        {showAddModal && <AddTaskModal onSave={handleAddTask} />}
+        {showAddModal && (
+          <AddTaskModal
+            onSave={handleAddEditTask}
+            taskToUpdate={taskToUpdate}
+          />
+        )}
         <div className="container">
           <SearchTask />
           <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
             <TaskActions onAddClick={() => setShowAddModal(true)} />
-            <TaskList tasks={tasks} />
+            <TaskList tasks={tasks} onEdit={handleEditTask} />
           </div>
         </div>
       </section>
